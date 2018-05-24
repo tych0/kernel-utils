673ee6f770751e8af58b76d07ff1f5234b322078
Tycho Andersen <tycho@tycho.ws>
2018-05-24 08:34:05 -0600

    remove old cruft
    Signed-off-by: Tycho Andersen <tycho@tycho.ws>

--- nfs-test/clean-containers.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-juju ssh $2 lxc delete --force container || true
-juju ssh $3 lxc delete --force container || true
--- nfs-test/load.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-i=0
-while true; do
-    echo $i $(date) >> /nfs/load.txt
-    i=$(($i+1))
-    sleep 1s
-done
